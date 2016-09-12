2964c2b558900e07fe0ca69a325f96341bef682e
the3noname <arens@arpemedia.com>
2016-09-12 09:40:00 +0200

    Rename apt constant and add a faster APT_HOOK implementation

--- blocks/packages/packages.go
+++ blocks/packages/packages.go
@@ -2,6 +2,7 @@ package packages
 
 import (
 	"bytes"
+	"io/ioutil"
 	"os/exec"
 	"strings"
 	"text/template"
@@ -13,8 +14,18 @@ import (
 type PackageManager int
 
 const (
-	// APTITUDE package manager (Debian, Ubuntu ...)
-	APTITUDE PackageManager = iota
+	// APT package manager (Debian, Ubuntu ...)
+	// This should work on all Debian based distributions out of
+	// the box.
+	APT PackageManager = iota
+	// APT_HOOK is faster than the APT version above, but depends
+	// on a change in the apt configuration
+	// 
+	// The line
+	//     DPkg::Post-Invoke-Success { '/usr/lib/update-notifier/apt-check &> /var/usr/updates';};
+	// needs to be added either into /etc/apt/apt.conf or one of 
+	// the existing files or a new file in /etc/apt/apt.conf.d/.
+	APT_HOOK
 	// PACMAN (Arch)
 	PACMAN
 )
@@ -36,19 +47,25 @@ func New() Packages {
 }
 
 // Exec collects the information
+// Check the PackageManager constant to learn about supported package managers.
 func (p *Packages) Exec(pkg PackageManager) error {
 	switch pkg {
-	case APTITUDE:
+	case APT:
 		out, err := exec.Command("/usr/lib/update-notifier/apt-check").CombinedOutput()
 		if err != nil {
 			return errors.Wrap(err, "executing command failed")
 		}
 
-		if len(out) == 0 {
-			return errors.New("CMD returned empty")
+		p.Data.Packages, err = parseAPT(out)
+		return errors.Wrap(err, "parsing output failed")
+	case APT_HOOK:
+		out, err := ioutil.ReadFile("/var/usr/updates")
+		if err != nil {
+			return errors.Wrap(err, "reading updates file failed")
 		}
-
-		p.Data.Packages = strings.Split(string(out), ";")[0]
+		
+		p.Data.Packages, err = parseAPT(out)
+		return errors.Wrap(err, "parsing output failed")
 	default:
 		return errors.New("Invalid package manager")
 	}
@@ -67,4 +84,12 @@ func (p *Packages) Print() (string, error) {
 	var out bytes.Buffer
 	err = t.Execute(&out, p.Data)
 	return out.String(), errors.Wrap(err, "executing template failed")
+}
+
+func parseAPT(b []byte) (string, error) {
+	if len(b) == 0 {
+		return "", errors.New("empty data body")
+	}
+
+	return strings.Split(string(out), ";")[0], nil
 }
